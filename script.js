ea3371a86ef7836b87fabf3408f26b32adab25c8
Inc0nsist3nt <waqaryunus992@gmail.com>
2026-05-29 08:28:01 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,23 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient(){
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+    css.textContent = body.style.background + ";";// will show the css code in the h3 element
+}
+setGradient();// to set the default background when the page loads
+// color1.addEventListener("input",setGradient);
+// color2.addEventListener("input",setGradient);
+
+function randomColors(){
+    color1.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    color2.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setGradient();
+}
